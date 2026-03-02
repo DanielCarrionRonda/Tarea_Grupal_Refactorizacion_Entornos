e0fa2bf94c0fa3998da3702c4437003e7c288f97
SergioSánchez <2865639@alu.murciaeduca.es>
2026-03-02 18:30:52 +0100

    Este es el push en el que esta todo terminado menos la parte de pablo

--- Mago.cs
+++ Mago.cs
@@ -2,6 +2,9 @@ using System;
 
 namespace CIFPCarlosIII.ED.UT04.Grupo3
 {
+    /// <summary>
+    /// Esta es la clase del Mago, lo primero es un constructor que hereda de Personaje
+    /// </summary>
     public class Mago : Personaje
     {
         private string nombre;
@@ -22,6 +25,10 @@ namespace CIFPCarlosIII.ED.UT04.Grupo3
             this.mana = 100;
             this.hechizo = "Bola de fuego";
         }
+        /// <summary>
+        /// Esto es un metodo encargado del ataque del mago, en caso de tener mana lo podra realizar
+        /// en caso de que no tenga mana, el ataque no se podria realizar.
+        /// </summary>
 
         public void LanzarHechizo()
         {
@@ -37,6 +44,9 @@ namespace CIFPCarlosIII.ED.UT04.Grupo3
             }
         }
 
+        /// <summary>
+        /// Método que hace que el mago para recuperar mana,en caso de tener 100 o mas no añadira nada.
+        /// </summary>
         public void Meditar()
         {
             mana += 30;
--- Personaje.cs
+++ Personaje.cs
@@ -32,6 +32,54 @@ namespace CIFPCarlosIII.ED.UT04.Grupo3
             this.vida = 100;
             this.nivel = 1;
         } 
+        /// <summary>
+        /// Método que devuelve la informacion principal de cada personaje dependiendo de su clase.
+        /// </summary>
+        public string MostrarInfo(Guerrero guerrero)
+       {
+           return "=== INFORMACIÓN DEL PERSONAJE ===" +
+                  "\nNombre: " + nombre +
+                  "\nClase: " + this.GetType().Name +
+                  "\nNivel: " + nivel +
+                  "\nVida: " + vida +
+                  "\nFuerza: " + guerrero.fuerza +
+                  "\nArma: " + guerrero.arma;
+
+
+       }
+       public string MostrarInfo(Mago mago)
+       {
+           return "=== INFORMACIÓN DEL PERSONAJE ===" +
+                  "\nNombre: " + nombre +
+                  "\nClase: " + this.GetType().Name +
+                  "\nNivel: " + nivel +
+                  "\nVida: " + vida +
+                  "\nMana: " + mago.mana +
+                  "\nHechizo: " + mago.hechizo;
+       }
+       public string MostrarInfo(Nigromante nigromante)
+       {
+           return "=== INFORMACIÓN DEL PERSONAJE ===" +
+                  "\nNombre: " + nombre +
+                  "\nClase: " + this.GetType().Name +
+                  "\nNivel: " + nivel +
+                  "\nVida: " + vida +
+                  "\nMana: " + nigromante.mana +
+                  "\nHechizo: " + nigromante.hechizo +
+                  "\nAlmas Capturadas: " + nigromante.almasCapturadas;
+       }
+       public string MostrarInfo(Berserker berserker)
+       {
+           return "=== INFORMACIÓN DEL PERSONAJE ===" +
+                  "\nNombre: " + nombre +
+                  "\nClase: " + this.GetType().Name +
+                  "\nNivel: " + nivel +
+                  "\nVida: " + vida +
+                  "\nFuerza: " + berserker.fuerza +
+                  "\nArma: " + berserker.arma +
+                  "\nFuria: " + berserker.furia;
+       }
+
 
         /// <summary>
         /// Método que indica quién ha realizado un ataque básico.
